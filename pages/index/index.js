ed5c263bb49973a9dbe3c3d980a0b42689dca33f
lyichao <931667248@qq.com>
2020-05-11 17:17:16 +0800

    首页UI切图

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,21 @@ Page({
       {uri:"https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png"},
       {uri:"https://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png"},
       {uri:"https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png"},
-    ]
+    ],
+      itemList:[
+          {}
+      ]
   },
+
+     /**
+    * 监听用户滑动页面事件。
+    * 请避免在 onPageScroll 中过于频繁的执行 setData 等引起逻辑层-渲染层通信的操作。
+    */
+   onPageScroll: function(e) {
+
+    console.log('触发onPageScroll事件')
+    //动态设置滑动到顶部的时长，最近最远速度相差无几，演示效果暂不考虑性能。
+  
+ }
 
 })
